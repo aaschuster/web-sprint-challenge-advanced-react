efe6fbeea26617b1b205ddaea1f285db64c594e7
aaschuster <aaronjschuster@gmail.com>
2023-01-24 14:59:21 -0500

    implement functionality of directional buttons for functional component

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -4,7 +4,7 @@ import React, {useState, useEffect} from 'react'
 const initialMessage = ''
 const initialEmail = ''
 const initialSteps = 0
-const initialIndex = 0 // the index the "B" is at
+const initialIndex = 4 // the index the "B" is at
 
 export default function AppFunctional(props) {
   const [idx, setIdx] = useState(initialIndex);
@@ -60,7 +60,7 @@ export default function AppFunctional(props) {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
 
-
+    setIdx(getNextIndex(evt.target.textContent.toLowerCase()));
   }
 
   function onChange(evt) {
@@ -71,8 +71,6 @@ export default function AppFunctional(props) {
     // Use a POST request to send a payload to the server.
   }
 
-  console.log(getNextIndex("down"));
-
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
@@ -81,9 +79,9 @@ export default function AppFunctional(props) {
       </div>
       <div id="grid">
         {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
+          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(i => (
+            <div key={i} className={`square${i === idx ? ' active' : ''}`}>
+              {i === idx ? 'B' : null}
             </div>
           ))
         }
@@ -92,10 +90,10 @@ export default function AppFunctional(props) {
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
+        <button id="left" onClick={move}>LEFT</button>
+        <button id="up" onClick={move}>UP</button>
+        <button id="right" onClick={move}>RIGHT</button>
+        <button id="down" onClick={move}>DOWN</button>
         <button id="reset">reset</button>
       </div>
       <form>
