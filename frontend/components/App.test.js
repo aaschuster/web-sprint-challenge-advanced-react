b0b3888168b227d4fb436ec3de0a14498516a6c7
aaschuster <aaronjschuster@gmail.com>
2023-01-27 14:18:31 -0500

    build out first few tests in App.test.js

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,43 @@
-// Write your tests here
+import { render } from "@testing-library/react"
+import React from "react";
+import AppClass from "./AppClass";
+import AppFunctional from "./AppFunctional";
+
+
 test('sanity', () => {
   expect(true).toBe(true)
+})
+
+test("class based app renders without errors", () => {
+  render(<AppClass />);
+})
+
+test("functional app renders without errors", () => {
+  render(<AppFunctional />);
+})
+
+test("functional app is functional", () => {
+  expect(AppFunctional.prototype && AppFunctional.prototype.isReactComponent)
+    .not.toBeTruthy();
+});
+
+test("class based app is class based", () => {
+  expect(AppClass.prototype && AppClass.prototype.isReactComponent)
+    .toBeTruthy();
+});
+
+test("ClassBased - active square is initially at index 4", () => {
+  render(<AppClass/>);
+
+  const squares = document.querySelectorAll(".square");
+  expect(squares[4].textContent).toBe("B");
+  expect(squares[4].className).toMatch(/active/);
+})
+
+test("Functional - active square is initially at index 4", () => {
+  render(<AppFunctional />);
+
+  const squares = document.querySelectorAll(".square");
+  expect(squares[4].textContent).toBe("B");
+  expect(squares[4].className).toMatch(/active/);
 })
